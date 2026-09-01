e55f6793d8bf12018644cbde224c14e4b23b16a7
Ashish Singh <singhashish0404@gmail.com>
2026-09-01 23:27:31 +0530

    added transformation orchestration

---
+++ src/transform.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from src.utils.db import get_connection
+from src.utils.logger import get_logger
+
+SQL_DIR = Path("sql/transform")
+logger = get_logger(__name__)
+
+
+def run_transform():
+    logger.info("starting transformation layer")
+
+    sql_file = SQL_DIR / "player_ranking.sql"
+
+    logger.info(f"reading SQL FILE {sql_file}")
+
+    with open(sql_file,"r",encoding="utf-8") as f:
+        sql =f.read()
+
+    con = get_connection()
+
+    try:
+        con.execute(sql)
+        logger.info("Player Rankiing transformation completed successfully")
+    finally:
+        con.close()
+        logger.info("DuckDB connection closed ")
+
+
+if __name__ == "__main__":
+    run_transform()
